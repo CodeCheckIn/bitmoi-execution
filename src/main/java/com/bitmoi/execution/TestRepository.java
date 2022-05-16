6bd2d6f9ebd97aad5d22fefb87bfd2088ce76509
tak6831 <tak6831@playnomm.com>
2022-05-16 09:20:21 +0900

    git ignore

--- src/main/java/com/bitmoi/execution/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bitmoi.execution;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface TestRepository extends ReactiveCrudRepository<TestTable, Integer> {
-
-}
--- src/main/java/com/bitmoi/execution/service/ExecuteService.java
+++
@@ -1,8 +0,0 @@
-package com.bitmoi.execution.service;
-
-import com.bitmoi.execution.domain.Execute;
-import reactor.core.publisher.Mono;
-
-public interface ExecuteService {
-    Mono<Execute> save(Execute execute);
-}
