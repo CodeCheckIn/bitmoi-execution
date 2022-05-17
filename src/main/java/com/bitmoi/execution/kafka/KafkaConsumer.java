04c267b7ccf5b0fb2c7beefb9c6f5babaaae76fc
Bae <qpyu66@naver.com>
2022-05-17 21:10:53 +0900

    add consumer config

--- src/main/java/com/bitmoi/execution/kafka/KafkaConsumer.java
+++ src/main/java/com/bitmoi/execution/kafka/KafkaConsumer.java
@@ -9,6 +9,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
@@ -19,25 +22,14 @@ public class KafkaConsumer{
 
     @Autowired
     OrderHandler orderHandler;
-//    @KafkaListener(topics = "order-bid", groupId = "bitmoi")
-//    public void consumeBid(String message) {
-//        logger.info(String.format("Consumed Bid message -> %s", message));
-//    }
-//
-//    @KafkaListener(topics = "order-ask", groupId = "bitmoi")
-//    public void consumeAsk(String message) {
-//        logger.info(String.format("Consumed Ask message -> %s", message));
-//    }
-    @KafkaListener(topics = "order", groupId = "bitmoi")
-    public Mono<Execute> consumeOrder(String message) {
-        logger.info(String.format("Consumed Ask message -> %s", message));
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            Order order = mapper.readValue(message, Order.class);
-            return orderHandler.getOrder(order);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        return null;
+
+    @KafkaListener(topics = "bitmoi-order", groupId = "bitmoi")
+    public void consume(@Headers MessageHeaders headers, @Payload Object payload) {
+        logger.info("CONSUME HEADERS : " + headers.toString());
+        logger.info("CONSUME PAYLOAD : " + payload);
+        System.out.println("CONSUME HEADERS : " + headers.toString());
+        System.out.println("CONSUME PAYLOAD : " + payload);
     }
+
+
 }
