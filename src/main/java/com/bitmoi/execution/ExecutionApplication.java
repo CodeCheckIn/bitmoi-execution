32fe2ffddb3830c1ebeecc5cb9babdc62af88692
radic2510 <jykim@atones.co.kr>
2022-05-17 21:14:25 +0900

    feat : eureka 활성화

--- src/main/java/com/bitmoi/execution/ExecutionApplication.java
+++ src/main/java/com/bitmoi/execution/ExecutionApplication.java
@@ -2,10 +2,12 @@ package com.bitmoi.execution;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
 @EnableR2dbcRepositories
+@EnableDiscoveryClient
 public class ExecutionApplication {
 
 	public static void main(String[] args) {
