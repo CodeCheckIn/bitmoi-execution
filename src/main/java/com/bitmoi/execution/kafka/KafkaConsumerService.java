75a9dfd202eef7c4b3834c1b7d2c0de28bf22bb9
tak6831 <tak6831@playnomm.com>
2022-05-19 09:24:05 +0900

    Fix : Chnage Consumerservice
    link get Order

--- src/main/java/com/bitmoi/execution/kafka/KafkaConsumerService.java
+++ src/main/java/com/bitmoi/execution/kafka/KafkaConsumerService.java
@@ -3,6 +3,8 @@ package com.bitmoi.execution.kafka;
 import com.bitmoi.execution.domain.Coin;
 import com.bitmoi.execution.domain.Order;
 import com.bitmoi.execution.handler.BatchHandler;
+import com.bitmoi.execution.handler.OrderHandler;
+import com.netflix.discovery.converters.Auto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,14 +14,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaConsumerService {
     private final Logger logger = LoggerFactory.getLogger(KafkaConsumerService.class);
+
     @Autowired
     BatchHandler batchHandler;
+
+    @Autowired
+    OrderHandler orderHandler;
+
     @KafkaListener(topics = "bitmoi-quotation", containerFactory = "coinConcurrentKafkaListenerContainerFactory")
     public void consume(Coin coin) {
         batchHandler.getBatch(coin);
     }
+
     @KafkaListener(topics = "bitmoi-order", containerFactory = "orderConcurrentKafkaListenerContainerFactory")
     public void consume(Order order) {
-        System.out.printf("[order] '%s %s %s \n", order.getCoinid(), order.getPrice(), order.getQuantity());
+        orderHandler.getOrder(order);
+//        System.out.printf("[order] '%s %s %s \n", order.getCoinid(), order.getPrice(), order.getQuantity());
     }
 }
